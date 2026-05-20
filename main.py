e4499524873fa6d492b8bf2cf5aff0af4b8b9c2f
Oskar <oskarrrbb@gmail.com>
2026-05-20 18:05:31 +0200

    road turning fixes

--- main.py
+++ main.py
@@ -18,20 +18,18 @@ road_height = screen_height - horizon_y
 x_center = screen_width / 2
 
 offset = 0
-distance_traveled=0
-
+distance_traveled = 0
+turn = 0
 map_lsit = 20 * [0] + 30 * [-1] + 100 * [0] + 100 * [1] + 100 * [0]
-def width_calc(i, offset,curve_type,current_x_center):
-    
-    
-    
+
+def width_calc(i, offset, curve_type, current_x_center):
     z_bottom = (number_of_road_segments - i) - offset + 1.0
     z_top = (number_of_road_segments - (i + 1)) - offset + 1.0
 
-    if z_bottom<=0.1:
-        z_bottom=0.1
-    if z_top<=0.1:
-        z_top=0.1
+    if z_bottom <= 0.1:
+        z_bottom = 0.1
+    if z_top <= 0.1:
+        z_top = 0.1
 
     scale = 1.0 / z_bottom
     scale_top = 1.0 / z_top
@@ -43,9 +41,9 @@ def width_calc(i, offset,curve_type,current_x_center):
     y_top = horizon_y + (road_height * scale_top)
 
     left_bottom_x = current_x_center - w / 2
-    right_bottom_x =  current_x_center + w / 2
-    left_top_x =  current_x_center - w_2 / 2
-    right_top_x =  current_x_center + w_2 / 2
+    right_bottom_x = current_x_center + w / 2
+    left_top_x = current_x_center - w_2 / 2
+    right_top_x = current_x_center + w_2 / 2
 
     p_1 = (int(left_bottom_x), int(y_bottom))
     p_2 = (int(right_bottom_x), int(y_bottom))
@@ -55,52 +53,68 @@ def width_calc(i, offset,curve_type,current_x_center):
     return p_1, p_2, p_3, p_4
 
 
-def draw_road():
-    
+def draw_road(turn):
     centers = [0] * number_of_road_segments
-    
     current_x = x_center
     dx = 0  
 
-    
     for i in range(number_of_road_segments - 1, -1, -1):
-        
-        
         map_index = distance_traveled + (number_of_road_segments - 1 - i)
         curve_type = map_lsit[map_index % len(map_lsit)]
-        
         
         dx += curve_type * 0.4    
         current_x += dx           
         
         centers[i] = current_x
 
-    
     for i in range(number_of_road_segments):
-        
+
         map_index = distance_traveled + (number_of_road_segments - 1 - i)
         curve_type = map_lsit[map_index % len(map_lsit)]
         
+        
+        z = (number_of_road_segments - i) - offset + 1.0
+        if z <= 0.1: z = 0.1
+        scale = 1.0 / z
+        
+        steering_projection = turn * (scale * 30) 
+        
+        final_center = centers[i] + steering_projection
+
         color_1 = (128, 128, 128)
         color_2 = (59, 59, 59)
         color = color_1 if map_index % 2 == 0 else color_2
 
-        p_1, p_2, p_3, p_4 = width_calc(i, offset, curve_type, centers[i])
+        p_1, p_2, p_3, p_4 = width_calc(i, offset, curve_type, final_center)
         pos = (p_1, p_2, p_4, p_3)
 
         pygame.draw.polygon(screen, color, pos)
 
+
 running = True
-
 while running:
-
     keys = pygame.key.get_pressed()
+    current_tile_curve = map_lsit[distance_traveled % len(map_lsit)]
+
+    
     if keys[pygame.K_w]:
         offset += 0.09  
+        turn -= current_tile_curve * 0.08
+
+    if keys[pygame.K_a]:
+        turn -= 0.15  
+    if keys[pygame.K_d]:
+        turn += 0.15  
+
+    
+    if turn < -4:
+        turn = -4
+    if turn > 4:
+        turn = 4
 
     if offset >= 1.0:
         offset -= 1.0
-        distance_traveled=distance_traveled+1
+        distance_traveled = distance_traveled + 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -108,8 +122,8 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
 
-    screen.fill((200, 20, 0))  
-    draw_road()
+    screen.fill((34, 139, 34))
+    draw_road(turn)
     pygame.display.flip()
     clock.tick(60)
 
